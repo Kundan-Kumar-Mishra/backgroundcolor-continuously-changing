257796980c73228b7b42a5b05e392a60aeceb742
kundan <kundan9091.kk@gmail.com>
2024-02-02 22:12:33 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Randomly changing background color 
+  The entire HTML body background color will change continuously.
+<br/>
+<br/>
+
+# Programming Language:
+ - html
+ - css 
+ - javascript 
+
+<br/>
+
+## How to use:
+- if you click on **Start** button background color will change continuously.
+- if you click on **Stop** button the will stop.
+
+<br/>
+
+
+## Learning:
+ - Understand the concept of callback
+ - Using the setInterval and clearInterval function
+ - Also using queryselector to select the element and radom() method to generate random number.
---
+++ script.js
@@ -0,0 +1,23 @@
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const hexNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+let intervelId;
+
+function randomColor() {
+    let hexColor = "#";
+    for (let i = 1; i <= 6; i++) {
+        let random = Math.floor(Math.random() * 16);
+        hexColor = hexColor + hexNum[random];
+       
+    }
+    document.body.style.backgroundColor = hexColor;
+}
+
+start.addEventListener("click", function () {
+    intervelId = setInterval(randomColor, 1000);
+
+})
+
+stop.addEventListener("click",function() {
+    clearInterval(intervelId);
+})
